4e2540224f637186987c12e26f1d2186561c7449
Sergei Cherniaev <sergei@cherniaev.com>
2023-11-16 00:07:05 -0500

    add binary search

---
+++ binary-search.go
@@ -0,0 +1,17 @@
+package main
+
+func search(nums []int, target int) int {
+	var lo, hi int = 0, len(nums) - 1
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if nums[mid] >= target {
+			hi = mid
+		} else {
+			lo = mid + 1
+		}
+	}
+	if nums[lo] == target {
+		return lo
+	}
+	return -1
+}
