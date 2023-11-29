0d7fe29b1c5593266ee0b6d57a58a6058b4bb535
Sergei Cherniaev <sergei@cherniaev.com>
2023-11-29 00:41:10 -0500

    optimize product of array except self solution

--- product-of-array-except-self.go
+++ product-of-array-except-self.go
@@ -1,44 +1,25 @@
 package main
 
 func productExceptSelf(nums []int) []int {
-	var prefix, postfix []int
-
-	prefix = append(prefix, nums[0])
-	for i := 1; i < len(nums); i++ {
-		prefix = append(prefix, nums[i]*prefix[i-1])
-	}
-
-	postfix = append(postfix, nums[len(nums)-1])
-	for i := len(nums) - 2; i >= 0; i-- {
-		postfix = append(postfix, nums[i]*postfix[len(postfix)-1])
-	}
-
-	reverse(postfix)
-
 	ans := []int{}
 
-	for i := range nums {
+	prefix := 1
+	for i := 0; i < len(nums)-1; i++ {
 		if i == 0 {
-			ans = append(ans, postfix[i+1])
-			continue
+			ans = append(ans, prefix)
 		}
-		if i == len(nums)-1 {
-			ans = append(ans, prefix[i-1])
-			continue
-		}
-		ans = append(ans, prefix[i-1]*postfix[i+1])
+		prefix *= nums[i]
+		ans = append(ans, prefix)
+	}
+
+	prefix = 1
+	for i := len(nums) - 1; i > 0; i-- {
+		prefix *= nums[i]
+		ans[i-1] *= prefix
 	}
 
 	return ans
 }
 
-// in place slice reverse function
-func reverse(s []int) {
-	l := len(s) - 1
-	for i := 0; i <= l/2; i++ {
-		s[i], s[l-i] = s[l-i], s[i]
-	}
-}
-
 // Time: O(n)
-// Space: O(n)
+// Space: O(1) output array isn't considered as extra memory in this problem
